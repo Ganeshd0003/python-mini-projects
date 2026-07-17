773f548a4324666f7d10cb2a8de5dc7d3a38cd12
Ganesh Dabhade <ganeshdabhade9689@gmail.com>
2026-07-17 23:13:28 +0530

    my api key removed

--- 4_News_Api/main.py
+++ 4_News_Api/main.py
@@ -1,6 +1,6 @@
 import requests
 query = input("Enter they keyword you wnat to search the news:")
-key = "put_api_key"
+key = "put_api_key_here"
 
 url = f"https://newsapi.org/v2/everything?q={query}&from=2025-12-18&sortBy=publishedAt&apiKey={key}"
 
